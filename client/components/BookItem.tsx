791b1958fe1b7fce773a3da7cdcad48d86ff1f9d
JSVenger <peteryar19@gmail.com>
2022-08-03 18:29:06 +0100

    prettier error breaking deployment fix

--- client/components/BookItem.tsx
+++ client/components/BookItem.tsx
@@ -6,9 +6,7 @@ function BookItem({ book, showDescription }: Props) {
   return (
     <Link href="/books/[id]" as={`/books/${book.id}`}>
       <span data-testid={`book-item${book.id}`} className={styles.bookItem}>
-        {showDescription?<h2>
-          {book.title} &rarr;
-        </h2>:<h2>{book.title}</h2>}
+        {showDescription ? <h2>{book.title} &rarr;</h2> : <h2>{book.title}</h2>}
         {showDescription && <p>{book.metaDescription}</p>}
       </span>
     </Link>
